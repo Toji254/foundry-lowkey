5fd93c84f167e003f501642e45904edb21ab7df7
ORINA, Cyrus Mogeni <fushi8631@gmail.com>
2026-09-24 16:04:13 +0300

    Remove walkthrough formatter debug

--- tests/test_walkthrough.py
+++ tests/test_walkthrough.py
@@ -325,9 +325,7 @@ class WalkthroughTests(unittest.TestCase):
             self.assertIn("current broadcast deployment", source)
 
     def test_human_error_explains_and_recommends(self):
-        debug = walk._friendly_error("StakeTokenNotAllowed()", "")
-        print("DEBUG_FRIENDLY", repr(walk.__file__), repr(debug), repr(walk._friendly_error.__code__.co_firstlineno))
-        message, recommendation = debug
+        message, recommendation = walk._friendly_error("StakeTokenNotAllowed()", "")
         self.assertIn("not currently approved", message)
         self.assertIn("approve the token", recommendation)
 
